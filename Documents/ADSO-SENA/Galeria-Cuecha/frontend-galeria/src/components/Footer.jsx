3fb19d33ecd6f26bb2e456f1ea2e6e46cb21645b
Daniela <dannyc8522@gmail.com>
2026-08-12 20:16:38 -0500

    Se crea el componente Footer.

---
+++ Documents/ADSO-SENA/Galeria-Cuecha/frontend-galeria/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+//Componente para mostrar pié de página.
+function Footer(){
+    return(
+        <footer className="footer">
+            <p>© 2026 Galería Cuecha - Todos los derechos reservados.</p>
+            {/*Contenedor con los enlaces de las redes sociales.*/}
+        </footer>
+    );
+}
+export default Footer;
