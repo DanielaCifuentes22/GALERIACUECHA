a347605d3ff6d74346fb049f620ae6bec6c8d8cb
Daniela <dannyc8522@gmail.com>
2026-08-23 16:49:55 -0500

    Crear servidor Express

---
+++ backend/src/server.js
@@ -0,0 +1,29 @@
+// Importa Express para crear el servidor
+const express = require("express");
+
+// Importa CORS para permitir solicitudes desde el frontend
+const cors = require("cors");
+
+// Crea una instancia de Express
+const app = express();
+
+// Permite que el servidor reciba datos en formato JSON
+app.use(express.json());
+
+// Permite la comunicación entre el frontend y el backend
+app.use(cors());
+
+// Ruta de prueba para verificar que el servidor está funcionando
+app.get("/", (req, res) => {
+    res.json({
+        mensaje: "API Galería Cuecha funcionando correctamente"
+    });
+});
+
+// Define el puerto en el que funcionará el servidor
+const PORT = 3000;
+
+// Inicia el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+});
